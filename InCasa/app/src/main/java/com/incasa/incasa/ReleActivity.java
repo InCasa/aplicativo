6787fefd84325bc253174e0dbd1ca3f0dc18ac9d
LeonardoCalado <leonardosilva.uba@hotmail.com>
2016-08-30 10:51:51 -0300

    Adicionado activitys, funcionalidades de menu e tela home

---
+++ InCasa/app/src/main/java/com/incasa/incasa/ReleActivity.java
@@ -0,0 +1,13 @@
+package com.incasa.incasa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rele);
+    }
+}
