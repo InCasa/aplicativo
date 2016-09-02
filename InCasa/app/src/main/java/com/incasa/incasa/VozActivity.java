1901a74884a45e5192a0beffd93dbd73f6e81153
LeonardoCalado <leonardosilva.uba@hotmail.com>
2016-09-02 08:56:07 -0300

    Modificando textos de mensagem

--- InCasa/app/src/main/java/com/incasa/incasa/VozActivity.java
+++ InCasa/app/src/main/java/com/incasa/incasa/VozActivity.java
@@ -25,6 +25,7 @@ public class VozActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voz);
 
